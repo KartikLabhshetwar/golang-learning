fb81989bccb9a80300eb7cd370f2c9556e51e7c0
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2025-03-31 20:28:40 +0530

    made a custom package in go

--- hello/main.go
+++ hello/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"hello/util"
+)
 
 func main() {
 	fmt.Println("this is the first program in go")
+
+	util.PrintMessage("this is the first custom package in go")
 }
