c84801294c652458376c62530e1bd33fdd30ab50
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2025-05-19 14:56:01 +0530

    learned about post, put and delete method in go

--- hello/crud/main.go
+++ hello/crud/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 type Todo struct {
@@ -13,9 +16,100 @@ type Todo struct {
 	Completed bool   `json:"completed"`
 }
 
-func main() {
-	fmt.Println("learning about crud operations in go.")
+func performPostRequest() {
+	todo := Todo{
+		UserId:    22,
+		Title:     "kartik labhshetwar",
+		Completed: true,
+	}
 
+	//convert todo struct into JSON.
+	jsonData, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("error marshalling the data", err)
+		return
+	}
+
+	// convert json data to string.
+	jsonString := string(jsonData)
+
+	// convert string to Reader
+	jsonReader := strings.NewReader(jsonString)
+
+	myURL := "https://jsonplaceholder.typicode.com/todos"
+
+	// send POST request
+
+	res, err := http.Post(myURL, "application/json", jsonReader)
+	if err != nil {
+		fmt.Println("error sending request", err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("error while reading the response", err)
+		return
+	}
+
+	fmt.Print(string(data))
+
+	fmt.Print(res.StatusCode)
+
+}
+
+func performUpdateRequest() {
+	todo := Todo{
+		UserId:    2323,
+		Title:     "hello world, I am here",
+		Completed: false,
+	}
+
+	// convert struct to JSON
+	jsonData, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("error while marshalling")
+	}
+
+	// convert JSON to string
+	jsonString := string(jsonData)
+
+	// convert string to reader
+	jsonReader := strings.NewReader(jsonString)
+
+	myURL := "https://jsonplaceholder.typicode.com/todos/1"
+
+	// Update method
+
+	req, err := http.NewRequest(http.MethodPut, myURL, jsonReader)
+	if err != nil {
+		fmt.Println("error doing put request", err)
+		return
+	}
+	req.Header.Set("Content-type", "application/json")
+
+	//send the request
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("error sending put request", err)
+		return
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("error reading response:", err)
+		return
+	}
+	fmt.Println(string(data))
+	fmt.Print(res.StatusCode)
+
+}
+
+func performGetRequest() {
 	//learning about get request in go
 
 	req, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
@@ -52,4 +146,36 @@ func main() {
 
 	fmt.Print("Todo:", todo)
 
+}
+
+func perfromDeleteRequest() {
+	myURL := "https://jsonplaceholder.typicode.com/todos/1"
+
+	// Delete method
+
+	req, err := http.NewRequest(http.MethodDelete, myURL, nil)
+	if err != nil {
+		fmt.Println("error while requesting", err)
+		return
+	}
+
+	//send the request
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("error sending put request", err)
+		return
+	}
+	defer res.Body.Close()
+
+	fmt.Println(res.Status)
+
+}
+
+func main() {
+	fmt.Println("learning about crud operations in go.")
+	// performGetRequest()
+	// performPostRequest()
+	// performUpdateRequest()
+	perfromDeleteRequest()
 }
